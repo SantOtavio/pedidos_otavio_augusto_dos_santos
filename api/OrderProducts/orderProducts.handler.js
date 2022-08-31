2a360ce4fac2ca613e2b3d17f66621c2f296bd43
Otavio Augusto Dos Santos <90930890+SantOtavio@users.noreply.github.com>
2022-08-30 21:52:11 -0300

    a

--- api/OrderProducts/orderProducts.handler.js
+++ api/OrderProducts/orderProducts.handler.js
@@ -6,7 +6,7 @@ async function orderProductsRegister(data = {
     Quantity: 0,
     OrderId: ""
 }) {
-    console.log("handler: ", data);
+    let id_IfIsEqualAnother = 0;
     if (!data.ProductId) {
         return { error: "0001", message: "Its necessary fill all requisition parameters!", necessaryFields: ["ProductId"] }
     }
@@ -19,25 +19,68 @@ async function orderProductsRegister(data = {
         return { error: "0001", message: "Its necessary fill all requisition parameters!", necessaryFields: ["Quantity"] }
     }
 
-    if(verifyOrder(data.OrderId) != true){
+    if (await verifyOrder(data.OrderId) != true) {
         return { error: "0005", message: "The order especified don't exist!", necessaryActions: ["Fill with a valid order id!"] }
     }
 
-    const orderProduct = await crud.save(tableName, undefined, data);
-    return (orderProduct);
+    if (await verifyIfOrderIsOpen(data.OrderId) != true) {
+        return { error: "0006", message: "The order especified is closed!", necessaryActions: ["Fill with a open order!"] }
+    }
+
+    if (await verifyIfHaveAnEqualProduct(data.ProductId) != false) {
+        let testObjet = await verifyIfHaveAnEqualProduct(data.ProductId);
+        data.Quantity = testObjet.quantity + data.Quantity
+        id_IfIsEqualAnother = testObjet.id;
+    }
+
+    if (id_IfIsEqualAnother > 0) {
+        const orderProduct = await crud.save(tableName, id_IfIsEqualAnother, data);
+        return (orderProduct);
+    } else {
+        const orderProduct = await crud.save(tableName, undefined, data);
+        return (orderProduct);
+    }
 }
 
-async function verifyOrder(OrderId){
+async function verifyOrder(OrderId) {
     const ordersArr = [];
     ordersArr.push(await crud.get("Orders"));
 
-    for(i = 0; i < ordersArr[0].length; i++){
-        if(ordersArr[0][i].id == OrderId){
+    for (i = 0; i < ordersArr[0].length; i++) {
+        if (ordersArr[0][i].id == OrderId) {
             return true;
         }
     }
 
     return false;
+}
+
+async function verifyIfOrderIsOpen(OrderId) {
+    const ordersArr = [];
+    ordersArr.push(await crud.get("Orders"));
+
+    for (i = 0; i < ordersArr[0].length; i++) {
+        if (ordersArr[0][i].id == OrderId) {
+            if (ordersArr[0][i].Status == "Open") {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+async function verifyIfHaveAnEqualProduct(ProductId) {
+    const orderProductsArr = [];
+    orderProductsArr.push(await crud.get("OrderProducts"));
+
+    for (i = 0; i < orderProductsArr[0].length; i++) {
+        if (orderProductsArr[0][i].ProductId == ProductId) {
+            return { quantity: orderProductsArr[0][i].Quantity, id: orderProductsArr[0][i].id }
+        }
+    }
+
+    return false
 }
 
 module.exports = {
